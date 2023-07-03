fb1ca004e12ac362a5645cec6b9bb62c443af941
adibwp <adwirawp@gmail.com>
2023-07-03 21:24:56 +0700

    add note db with sqflite

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 class Note {
   final int? id;
   final String title;
@@ -9,4 +10,40 @@ class Note {
     required this.description,
     required this.time,
   });
+
+  Note copyWith({
+    int? id,
+    String? title,
+    String? description,
+    DateTime? time,
+  }) {
+    return Note(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      time: time ?? this.time,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'title': title,
+      'description': description,
+      'time': time.millisecondsSinceEpoch,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      id: map['id'] != null ? map['id'] as int : null,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      time: DateTime.fromMillisecondsSinceEpoch(map['time'] as int),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Note.fromJson(String source) => Note.fromMap(json.decode(source) as Map<String, dynamic>);
 }
